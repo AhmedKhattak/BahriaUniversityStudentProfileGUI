e8e86461887d52c8379b41505f6ee9d14858397a
Khattak <Khattak.ahmed@yahoo.com>
2016-07-31 19:50:35 +0500

    added activity for changelog

---
+++ app/src/main/java/com/example/ahmed/redux/Activities/changelog.java
@@ -0,0 +1,9 @@
+package com.example.ahmed.redux.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Khattak on 7/30/2016.
+ */
+public class changelog  extends AppCompatActivity {
+}
