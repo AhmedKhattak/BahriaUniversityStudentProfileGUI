d27bcd1f345e12cc69974551d0c33abdb3eb4e6f
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-08-01 23:29:15 +0500

    added about activity with same back press overriden

---
+++ app/src/main/java/com/example/ahmed/redux/Activities/AboutActivity.java
@@ -0,0 +1,60 @@
+package com.example.ahmed.redux.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.example.ahmed.redux.R;
+
+/**
+ * Created by Ahmed Khattak on 02/08/2016.
+ */
+public class AboutActivity extends AppCompatActivity {
+
+
+    private Toolbar toolbar;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        toolbar = (Toolbar) findViewById(R.id.toolbarr);
+        toolbar.setTitle("About");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        toolbar=null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d("Fuck", "sht");
+                finish();
+                overridePendingTransition(R.anim.test2,android.R.anim.fade_out);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d("Fuck", "sht");
+        finish();
+        overridePendingTransition(R.anim.test2,android.R.anim.fade_out);
+    }
+}
+
