956959b63c07b2318578f0c7a76ec67259fcfde3
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-08-07 04:37:11 +0500

    model for navdrawer item

---
+++ app/src/main/java/com/example/ahmed/redux/Models/NavItems_Model.java
@@ -0,0 +1,22 @@
+package com.example.ahmed.redux.Models;
+
+/**
+ * Created by Ahmed Khattak on 05/08/2016.
+ */
+public class NavItems_Model {
+
+    private String id;
+
+    public NavItems_Model(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}
