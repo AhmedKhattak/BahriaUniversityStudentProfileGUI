7520a434ee9cceed6227de451e24b9e03a187796
Khattak <Khattak.ahmed@yahoo.com>
2016-08-12 10:30:48 +0500

    profile activity

---
+++ app/src/main/java/com/example/ahmed/redux/Activities/ProfileActivity.java
@@ -0,0 +1,73 @@
+package com.example.ahmed.redux.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.ahmed.redux.R;
+
+/**
+ * Created by Khattak on 8/10/2016.
+ */
+public class ProfileActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        toolbar = (Toolbar) findViewById(R.id.toolbarp);
+        toolbar.setTitle("Profile");
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        toolbar=null;
+        fab.setOnClickListener(null);
+        fab=null;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Log.d("Fuck", "sht");
+                finish();
+                overridePendingTransition(R.anim.nothing,R.anim.test4);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d("Fuck", "sht");
+        finish();
+        overridePendingTransition(R.anim.nothing,R.anim.test4);
+    }
+}
