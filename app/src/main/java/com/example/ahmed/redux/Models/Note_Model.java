258395242a5238c7997868cb4fc1011e83cd9372
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-08-17 13:18:51 +0500

    added note model

---
+++ app/src/main/java/com/example/ahmed/redux/Models/Note_Model.java
@@ -0,0 +1,20 @@
+package com.example.ahmed.redux.Models;
+
+/**
+ * Created by Ahmed Khattak on 15/08/2016.
+ */
+public class Note_Model {
+    private String note;
+
+    public Note_Model(String note) {
+        this.note = note;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+}
