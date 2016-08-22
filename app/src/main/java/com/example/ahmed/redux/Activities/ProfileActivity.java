bae344713af1e5412a22620384a39f7673aeb716
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-08-22 22:25:56 +0500

    Added Dialogs

--- app/src/main/java/com/example/ahmed/redux/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/ahmed/redux/Activities/ProfileActivity.java
@@ -9,7 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.example.ahmed.redux.R;
 
 /**
@@ -19,6 +22,9 @@ public class ProfileActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private FloatingActionButton fab;
+    LinearLayout linearLayout;
+    TextView textView;
+    TextView textView2;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +44,43 @@ public class ProfileActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        linearLayout=(LinearLayout)findViewById(R.id.passwordchange);
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(ProfileActivity.this)
+                        .title("Change Password")
+                        .customView(R.layout.dialog_change_password, true)
+                        .positiveText("Change Password")
+                        .negativeText("Go Back")
+                        .show();
+            }
+        });
+        textView=(TextView)findViewById(R.id.buemailpass);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(ProfileActivity.this)
+                        .title("BU Email Password")
+                        .customView(R.layout.dialog_bu_email_password_view, true)
+                        .positiveText("DONE")
+                        .show();
+            }
+        });
+
+        textView2=(TextView)findViewById(R.id.buemailpassagain);
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(ProfileActivity.this)
+                        .title("BU Email Password")
+                        .customView(R.layout.dialog_bu_email_password_view, true)
+                        .positiveText("DONE")
+                        .show();
+            }
+        });
+
     }
 
     @Override
@@ -46,6 +89,9 @@ public class ProfileActivity extends AppCompatActivity {
         toolbar=null;
         fab.setOnClickListener(null);
         fab=null;
+        linearLayout.setOnClickListener(null);
+        textView.setOnClickListener(null);
+        textView2.setOnClickListener(null);
     }
 
     @Override
