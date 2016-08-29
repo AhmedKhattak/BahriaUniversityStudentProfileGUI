af7183c2950e4d9785fa9a80d092d8557d7d250b
Ahmed Khattak <khattak.ahmed@yahoo.com>
2016-08-29 21:43:23 +0500

    global singleton

---
+++ app/src/main/java/com/example/ahmed/redux/Application/ApplicationGlobal.java
@@ -0,0 +1,10 @@
+package com.example.ahmed.redux.Application;
+
+import android.app.Application;
+
+/**
+ * Created by Ahmed Khattak on 26/08/2016.
+ */
+public class ApplicationGlobal extends Application {
+
+}
